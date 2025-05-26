5394e30e28ef204675144133811408656327a46b
gregandcin <gregandcinbusiness@gmail.com>
2025-05-26 13:56:42 -0400

    feat: update next event link

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@ const profile = reactive({
     {
       id: 2,
       title: "Next Beyblade Event!",
-      url: "https://worldbeyblade.org/Thread-HOMETOWN-HEROES-BEYBLADE-X-VOL-13--116905",
+      url: "https://worldbeyblade.org/Thread-HOMETOWN-HEROES-BEYBLADE-X-VOL-14--117114",
       icon: "game-icons:spinning-top",
       style: "btn-accent",
     },
